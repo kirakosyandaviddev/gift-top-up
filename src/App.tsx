db7b87676ca300e6304c42cb1abc337d22c63bc7
David <kirakosyandaviddev@gmail.com>
2025-03-24 23:48:24 +0400

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -29,14 +29,7 @@ function App() {
               Show Alert
           </button>
         </div>
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
