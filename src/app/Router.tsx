8eb60e8c6f4c4090eae4271f43df554ce26e4546
David <kirakosyandaviddev@gmail.com>
2025-03-26 01:04:35 +0400

    add routing

---
+++ src/app/Router.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import {HomePage} from "../pages/HomePage/HomePage";
+import {WelcomePage} from "../pages/WelcomePage/WelcomePage";
+import {RoulettePage} from "../pages/RoulettePage/RoulettePage";
+import {NotFoundPage} from "../pages/NotFoundPage/NotFoundPage";
+
+import {Layout} from "../components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <WelcomePage /> },
+      { path: 'main', element: <HomePage /> },
+      { path: 'roulette', element: <RoulettePage /> },
+    ],
+  },
+  { path: "*", element: <NotFoundPage /> },
+]);
+
+export const Router = () => {
+  return <RouterProvider router={router} />;
+}
---
+++ src/consts/routes.ts
@@ -0,0 +1,5 @@
+export const routes = {
+  welcome: '/',
+  home: '/main',
+  roulette: '/roulette',
+}
