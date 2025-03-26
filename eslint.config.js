7e88614851e01530d131c1c2d4f0657755fe87a2
David <kirakosyandaviddev@gmail.com>
2025-03-27 02:30:00 +0400

    remove airbnb eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,38 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import {defineConfig} from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {globals: globals.browser},
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { js },
-    extends: [
-      "airbnb",
-      "airbnb-typescript",
-      "airbnb/hooks",
-      "plugin:react/recommended",
-      "standard-with-typescript",
-    ],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {js},
+    extends: ['plugin:react/recommended', 'standard-with-typescript'],
     rules: {
       quotes: [
-        "error",
-        "single",
-        { avoidEscape: true, allowTemplateLiterals: true },
+        'error',
+        'single',
+        {avoidEscape: true, allowTemplateLiterals: true},
       ],
 
-      "react/jsx-quotes": ["error", "double"],
-      "import/imports-first": "error",
-      "object-curly-spacing": ["error", "never"],
+      'react/jsx-quotes': ['error', 'double'],
+      'import/imports-first': 'error',
+      'object-curly-spacing': ['error', 'never'],
     },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{jsx,tsx}"],
+    files: ['**/*.{jsx,tsx}'],
     rules: {
-      quotes: "off",
-      "react/jsx-quotes": ["error", "double"],
+      quotes: 'off',
+      'react/jsx-quotes': ['error', 'double'],
     },
   },
 ]);
