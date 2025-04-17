669a474a2218dd727aab4f9fdafea28174b47744
David <kirakosyandaviddev@gmail.com>
2025-04-18 00:30:16 +0400

    added new subscriptions

--- src/hooks/data/mutations/useSwapGiftToTonMutation.ts
+++ src/hooks/data/mutations/useSwapGiftToTonMutation.ts
@@ -1,35 +1,17 @@
-import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {useMutation} from '@tanstack/react-query';
 
 import {QUERY_KEYS} from '../../../consts/queryKeys';
 import {axiosClient} from '../../../libs/axiosClient';
 import {ENDPOINTS} from '../../../consts/endpoints';
 import {ResponseType} from '../../../etities/types/ResponseType';
 import {useWebApp} from '../../useWebApp';
-import {GetInfoResponseType} from '../queries/useGetInfo';
 
 type SwapGiftToTonResponseType = {
   balance: number;
 };
 
-const prepareInfo = (
-  cacheData: GetInfoResponseType,
-  data: SwapGiftToTonResponseType,
-): GetInfoResponseType => {
-  return {
-    ...cacheData,
-    data: {
-      ...cacheData.data,
-      user: {
-        ...cacheData.data.user,
-        balance: data.balance,
-      },
-    },
-  };
-};
-
 export const useSwapGiftToTonMutation = () => {
   const WebApp = useWebApp();
-  const queryClient = useQueryClient();
 
   const {data, mutate, isSuccess, isPending, error} = useMutation({
     mutationKey: [QUERY_KEYS.SWAP_GIFT_TO_TON],
@@ -42,15 +24,6 @@ export const useSwapGiftToTonMutation = () => {
         data: {initData: WebApp?.initData, id},
       });
       return response.data;
-    },
-    onSuccess: (d) => {
-      queryClient.setQueryData(
-        [QUERY_KEYS.GET_INFO],
-        (cacheData: GetInfoResponseType) => {
-          if (!cacheData) return cacheData;
-          return prepareInfo(cacheData, d.data);
-        },
-      );
     },
   });
 
