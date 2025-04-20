d473031097022728d39dd42783653ced1deb9565
David <kirakosyandaviddev@gmail.com>
2025-04-21 01:51:32 +0400

    fix icon

--- src/pages/RoulettePage/components/SwipeButton/SwipeButton.tsx
+++ src/pages/RoulettePage/components/SwipeButton/SwipeButton.tsx
@@ -164,7 +164,7 @@ export const SwipeButton = ({
           </textPath>
         </text>
 
-        <g transform="translate(260, 22) scale(0.9) rotate(6)">
+        <g transform="translate(255, 22) scale(0.9) rotate(6)">
           <path
             fillRule="evenodd"
             clipRule="evenodd"
